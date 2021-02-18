1b33aa96c4a0b4d8a810f38e3ca32de04976a9c8
Ruben Castillo <rcastillo@raivtech.com>
2021-02-18 16:37:37 -0700

    Se crea agenda de citas

---
+++ citas/src/App.js
@@ -0,0 +1,55 @@
+import React, { Fragment, useState } from 'react';
+import Formulario from './components/Formulario';
+import Cita from './components/Cita';
+
+
+function App() {
+
+  // Arreglo de citas
+
+  const [ citas, guardarCitas ] = useState([]);
+
+  // Función que tome las citas actuales y agregue la nueva
+
+  const crearCita = cita => {
+    guardarCitas([
+      ...citas,
+      cita
+    ]);
+  }
+
+  // Función que elimina una cita por su ID
+
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter( cita => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
+  return (
+   <Fragment>
+      <h1>Administrador de pacientes</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario 
+              crearCita={crearCita}
+            />
+          </div>
+          <div className="one-half column">
+            <h2>Administra tus citas</h2>
+            {citas.map(cita => (
+              <Cita 
+                key={cita.id}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+   </Fragment>
+  );
+}
+
+export default App;
