3e267d764252cc20558947c4c756e51af35272d1
Ruben Castillo <rcastillo@raivtech.com>
2021-02-23 17:49:17 -0700

    Se crea proyecto de presupuestos React

---
+++ presupuesto/src/components/Pregunta.js
@@ -0,0 +1,67 @@
+import React, { Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
+import Error from './Error';
+
+const Pregunta = ({ guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
+
+    //Definir el state
+
+    const [ cantidad, guardarCantidad ] = useState(0);
+    const [ error, guardarError ] = useState(false);
+
+    //Funcion que lee el presupuesto
+
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value, 10));
+    }
+
+    // Submit para definir presupuesto
+    const agregarPesupuesto = e => {
+        e.preventDefault();
+        
+        //Validar
+        if(cantidad < 1 || isNaN ( cantidad )) {
+            guardarError(true);
+            return;
+        } 
+
+
+        //Se pasa la validación
+
+        guardarError(false);
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+        actualizarPregunta(false);
+    }
+
+    return ( 
+        <Fragment>
+           <h2>Coloca tu presupuesto</h2>
+           { error ? <Error mensaje="El presupuesto es incorrecto" /> : null}
+           <form
+            onSubmit={agregarPesupuesto}
+           >
+                <input 
+                   type="number"
+                   className="u-full-width"
+                   placeholder="Coloca tu presupuesto" 
+                   onChange={definirPresupuesto}
+                />  
+                <input 
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Definir presupuesto"
+                /> 
+            </form> 
+        </Fragment>           
+     );
+}
+
+
+Pregunta.propTypes = {
+    guardarPresupuesto: PropTypes.func.isRequired,
+    guardarRestante: PropTypes.func.isRequired,
+    actualizarPregunta: PropTypes.func.isRequired
+}
+ 
+export default Pregunta;
